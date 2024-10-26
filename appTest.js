50e825409e3ad6cd4294c28945e66a0504131dd5
Miroslav Tsarev <mtzarev@gmail.com>
2024-10-26 23:35:18 +0300

    Update appTest.js

--- appTest.js
+++ appTest.js
@@ -1,18 +1,18 @@
 function appTest(input) {
     let restaurants = {}
     input.forEach(line => {
-        let separator = line.split(` - `)
+        let separator = line.split(` - `);
         let name = separator[0];
         let workersArr = separator[1].split(`, `);
        
 let workers=[];
 for(let worker of workersArr){
     let workerTokens = worker.split(` `);
-    let salary = Number(workerTokens[1])
-    workers.push({ name:workerTokens[0], salary })
+    let salary = Number(workerTokens[1]);
+    workers.push({ name:workerTokens[0], salary });
 }
 if (restaurants[name]){
-    workers = workers.concat(restaurants[name].workers)
+    workers = workers.concat(restaurants[name].workers);
 }
 workers.sort((a,b)=>b.salary-a.salary);
 let bestSalary=workers[0].salary;
